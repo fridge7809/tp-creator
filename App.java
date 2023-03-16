a914efa8246ecef8fb5296069917b350b5497cf0
fridge7809 <127931228+fridge7809@users.noreply.github.com>
2023-03-16 16:12:18 +0100

    n

--- App.java
+++ App.java
@@ -1,10 +1,8 @@
 class App {
     public static void main(String[] args) {
 		//Time timeModule = new Time();
-		User newUser = new User(300, 0, 0);
-		Run newSesh = new Session(34, "bige");
-		newUser.executeWorkout(newSesh);
-		System.out.println(newUser.atl);
-		System.out.println("hello world");
+        User chichi = new User(300, 0, 0);
+        Run newSesh = new Session(30, "Interval");
+        chichi.executeWorkout(newSesh);
     }
 }
